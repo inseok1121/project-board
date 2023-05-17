f9dd0b8e4aade52434ed30346f4200035b1ce094
SongP <inseck1121@gmail.com>
2023-05-17 21:16:41 +0900

    #15 - 게시글 뷰 테스트 내용 업데이트

--- src/test/java/com/project/projectboard/controller/ArticleControllerTests.java
+++ src/test/java/com/project/projectboard/controller/ArticleControllerTests.java
@@ -1,5 +1,6 @@
 package com.project.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,37 +20,46 @@ public class ArticleControllerTests {
         this.mvc = mvc;
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception{
 
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 (게시판) 페이지 - 정상 호출")
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception{
 
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(view().name("articleComments"))
                 .andExpect(model().attributeExists("articles"));
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 (게시판) 페이지 - 정상 호출")
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception{
 
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 전용 (게시판) 페이지 - 정상 호출")
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception{
 
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("articles/search-hashtag"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
