011d9333f37e4452a6415db928e024e0ac87cc89
SongP <inseck1121@gmail.com>
2023-05-26 21:38:01 +0900

    #15 - ArticleControllerTest 정상 동작 확인

--- src/test/java/com/project/projectboard/controller/ArticleControllerTests.java
+++ src/test/java/com/project/projectboard/controller/ArticleControllerTests.java
@@ -2,6 +2,7 @@ package com.project.projectboard.controller;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -21,6 +22,7 @@ public class ArticleControllerTests {
     }
 
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception{
 
         mvc.perform(get("/articles"))
@@ -31,18 +33,19 @@ public class ArticleControllerTests {
     }
 
     @DisplayName("[view][GET] 게시글 상세 (게시판) 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception{
 
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(view().name("articleComments"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
     @Disabled("구현 중")
-
     @DisplayName("[view][GET] 게시글 검색 전용 (게시판) 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception{
 
         mvc.perform(get("/articles/search"))
@@ -53,6 +56,7 @@ public class ArticleControllerTests {
 
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 전용 (게시판) 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception{
 
         mvc.perform(get("/articles/search-hashtag"))
